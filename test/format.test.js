aaba4032dda980f48c0310b244320bde69460914
teresam856 <tere486martinez@gmail.com>
2021-10-26 13:29:53 -0700

    fix formatting tests

--- test/format.test.js
+++ test/format.test.js
@@ -1,67 +1,66 @@
-// import fs from 'fs'
-// import tmp from 'tmp-promise'
+import fs from 'fs'
+import tmp from 'tmp-promise'
 
-// import { promisify } from 'es6-promisify'
-// import getStream from 'get-stream'
+import { promisify } from 'es6-promisify'
+import getStream from 'get-stream'
 
-// import gtf from '../src'
+import gtf from '../src'
 
-// const readfile = promisify(fs.readFile)
-// const fdatasync = promisify(fs.fdatasync)
+const readfile = promisify(fs.readFile)
+const fdatasync = promisify(fs.fdatasync)
 
 describe('GTF formatting', () => {
   it(`test formatSync/parseStringSync`, () => {
-    // const inputGTF = fs
-    //   .readFileSync(require.resolve(`./data/volvox.sorted.gtf`))
-    //   .toString('utf8')
+    const inputGTF = fs
+      .readFileSync(require.resolve(`./data/hybrid.gtf`))
+      .toString('utf8')
 
-    // const expectedGTF = fs
-    //   .readFileSync(require.resolve(`./data/hybrid.reformatted.gtf`))
-    //   .toString('utf8')
-    //   .replace(/###\n/g, '') // formatSync does not insert sync marks
+    const expectedGTF = fs
+      .readFileSync(require.resolve(`./data/hybrid.reformatted.gtf`))
+      .toString('utf8')
 
-    // const items = gtf.parseStringSync(inputGTF, { parseAll: true })
-    // const resultGTF = gtf.formatSync(items)
-    // expect(resultGTF).toEqual(expectedGTF)
+    const items = gtf.parseStringSync(inputGTF, { parseAll: true })
+    const resultGTF = gtf.formatSync(items)
+    expect(resultGTF).toEqual(expectedGTF)
     expect(true).toBe(true)
   })
 
-  // it(`test formatStream/formatStream`, async () => {
-  //   const expectedGTF = (
-  //     await readfile(require.resolve(`./data/hybrid.reformatted.gtf`))
-  //   ).toString('utf8')
+  it(`test formatStream/formatStream`, async () => {
+    const expectedGTF = (
+      await readfile(require.resolve(`./data/hybrid.reformatted.gtf`))
+    ).toString('utf8')
 
-  //   const resultGTF = await getStream(
-  //     fs
-  //       .createReadStream(require.resolve(`./data/hybrid.gtf`))
-  //       .pipe(
-  //         gtf.parseStream({
-  //           parseFeatures: true,
-  //           parseComments: true,
-  //           parseDirectives: true,
-  //         }),
-  //       )
-  //       .pipe(gtf.formatStream()),
-  //   )
-  //   expect(resultGTF).toEqual(expectedGTF)
-  // })
-  // it(`test formatFile/parseFile`, async () => {
-  //   jest.setTimeout(1000)
-  //   await tmp.withFile(async tmpFile => {
-  //     const gtfIn = fs
-  //       .createReadStream(require.resolve(`./data/demo2.gtf`))
-  //       .pipe(gtf.parseStream({ parseAll: true }))
+    const resultGTF = await getStream(
+      fs
+        .createReadStream(require.resolve(`./data/hybrid.gtf`))
+        .pipe(
+          gtf.parseStream({
+            parseFeatures: true,
+            parseComments: true,
+            parseDirectives: true,
+          }),
+        )
+        .pipe(gtf.formatStream()),
+    )
+    expect(resultGTF).toEqual(expectedGTF)
+  })
+  it(`test formatFile/parseFile`, async () => {
+    jest.setTimeout(1000)
+    await tmp.withFile(async tmpFile => {
+      const gtfIn = fs
+        .createReadStream(require.resolve(`./data/hybrid.gtf`))
+        .pipe(gtf.parseStream({ parseAll: true }))
 
-  //     await gtf.formatFile(gtfIn, tmpFile.path)
-  //     await fdatasync(tmpFile.fd)
+      await gtf.formatFile(gtfIn, tmpFile.path)
+      await fdatasync(tmpFile.fd)
 
-  //     const resultGTF = (await readfile(tmpFile.path)).toString('utf8')
+      const resultGTF = (await readfile(tmpFile.path)).toString('utf8')
 
-  //     const expectedGTF = (
-  //       await readfile(require.resolve(`./data/demo2.reformatted.gtf`))
-  //     ).toString('utf8')
+      const expectedGTF = (
+        await readfile(require.resolve(`./data/hybrid.reformatted.gtf`))
+      ).toString('utf8')
 
-  //     expect(resultGTF).toEqual(expectedGTF)
-  //   })
-  // })
+      expect(resultGTF).toEqual(expectedGTF)
+    })
+  })
 })
